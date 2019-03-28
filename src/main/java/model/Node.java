ff9ab978bf59e6a9a9092e50bfe2bd22a2544587
Tyler Ferrara <tylerferrara123@gmail.com>
2019-03-28 11:35:58 -0400

    created simple FloorMap implimentation

--- src/main/java/model/Node.java
+++ src/main/java/model/Node.java
@@ -13,6 +13,87 @@ public class Node {
         return true;
     }
 
+    public int getXcoord() {
+        return xcoord;
+    }
+
+    public void setXcoord(int xcoord) {
+        this.xcoord = xcoord;
+    }
+
+    public int getYcoord() {
+        return ycoord;
+    }
+
+    public void setYcoord(int ycoord) {
+        this.ycoord = ycoord;
+    }
+
+    public String getNodeID() {
+        return nodeID;
+    }
+
+    public void setNodeID(String nodeID) {
+        this.nodeID = nodeID;
+    }
+
+    public String getFloor() {
+        return floor;
+    }
+
+    public void setFloor(String floor) {
+        this.floor = floor;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public String getNodeType() {
+        return nodeType;
+    }
+
+    public void setNodeType(String nodeType) {
+        this.nodeType = nodeType;
+    }
+
+    public String getLongName() {
+        return longName;
+    }
+
+    public void setLongName(String longName) {
+        this.longName = longName;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public Node(int x, int y) {
+        xcoord = x;
+        ycoord = y;
+        nodeID = "" + x + y;
+    }
+
+    public Node(int xcoord, int ycoord, String nodeID, String floor, String building, String nodeType, String longName, String shortName) {
+        this.xcoord = xcoord;
+        this.ycoord = ycoord;
+        this.nodeID = nodeID;
+        this.floor = floor;
+        this.building = building;
+        this.nodeType = nodeType;
+        this.longName = longName;
+        this.shortName = shortName;
+    }
+
     public boolean validateType(String nodeType) {
         return true;
     }
--- src/main/java/model/Point.java
+++ src/main/java/model/Point.java
@@ -3,7 +3,55 @@ package model;
 public class Point {
 
     private double weight;
-    private Node node;
-    private Node next;
+    private int x, y;
+    private String nodeID;
+    private Point next;
 
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getNodeID() {
+        return nodeID;
+    }
+
+    public void setNodeID(String nodeID) {
+        this.nodeID = nodeID;
+    }
+
+    public Point getNext() {
+        return next;
+    }
+
+    public void setNext(Point next) {
+        this.next = next;
+    }
+
+    public Point(double weight, int x, int y, String nodeID, Point next) {
+        this.weight = weight;
+        this.x = x;
+        this.y = y;
+        this.nodeID = nodeID;
+        this.next = next;
+    }
 }
