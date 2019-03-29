2fb4a3079abb41933071e9589069697352a6d12a
samgoldman <sgoldman216@gmail.com>
2019-03-29 00:49:07 -0400

    Add MainTest
    This test verifies that a window opens on main() and that it does so in
    an appropriate amount of time

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,75 @@
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.testfx.api.FxRobot;
+import testclassifications.FastTest;
+
+import javafx.stage.Window;
+import testclassifications.UiTest;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+@Category(UiTest.class)
+public class MainTest {
+
+    private final int EXPECTED_INITIAL_WINDOWS = 1;
+    private final int ALLOWABLE_STARTUP_DELAY = 1000; // milliseconds
+
+    @Test
+    @Category(FastTest.class)
+    public void testMain() {
+
+        // This test serves two purposes:
+        // 1) It verifies that the correct number of windows initialize when main is run and,
+        // 2) It tests that the application startup time is adequate
+
+        // Must run Main in a separate thread as it blocks
+        Thread thread = new Thread(() -> {
+            try {
+                Main.main(new String[]{});
+            } catch (Exception e) {
+                // This is to catch InterruptedExceptions, which it doesn't technically throw
+            }
+        });
+        thread.start();
+
+        // Create a robot
+        // Note: this is not standard for UI tests. Normally the test class extends ApplicationTest, which provides
+        // a robot
+        FxRobot robot = new FxRobot();
+        List<Window> windows;
+
+        // Precondition: grab windows and verify that none exist yet
+        // Must catch the assertion so that we can kill the application thread, then manually fail.
+        try {
+            windows = robot.listWindows();
+            assertThat(windows.size(), is(0));
+        } catch (AssertionError ae) {
+            ae.printStackTrace();
+            thread.interrupt();
+            fail();
+        }
+
+        // Give the application a bit to startup
+        try {
+            Thread.sleep(ALLOWABLE_STARTUP_DELAY);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Test: verify that there are the current number of windows
+        try {
+            windows = robot.listWindows();
+            assertThat(windows.size(), is(EXPECTED_INITIAL_WINDOWS));
+        } catch (AssertionError ae) {
+            ae.printStackTrace();
+            thread.interrupt();
+            fail();
+        }
+
+        // Kill the application thread
+        thread.interrupt();
+    }
+}
