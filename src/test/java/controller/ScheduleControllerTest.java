bb893440d1d67a1eb6e8d8ee4542967ce78dd3fd
Lauren Hatfield <lhatfield15@gmail.com>
2019-03-29 17:14:48 -0400

    added test categories

--- src/test/java/controller/ScheduleControllerTest.java
+++ src/test/java/controller/ScheduleControllerTest.java
@@ -30,39 +30,45 @@ public class ScheduleControllerTest {
         rooms.clear();
     }
 
-
+    @Category(FastTest.class)
     @Test
     public void testGetMaxPeopleContent(){//has all the right rooms
         assertThat(sc.getMaxPeople(20), containsInAnyOrder(rooms));
     }
 
+    @Category(FastTest.class)
     @Test
     public void testGetMaxPeopleSize(){//does not have more rooms than expected(test limitation from maxPeopleContent
         assertThat(sc.getMaxPeople(20).size(), equalTo(5));
     }
 
+    @Category(FastTest.class)
     @Test
     public void getDay(){
         assertThat(sc.getDay(), equalTo("12122019"));
     }
 
+    @Category(FastTest.class)
     @Test
     public void getRoom() {
         assertThat(sc.getRoom(), equalTo("ROOM1"));
     }
 
+    @Category(FastTest.class)
     @Test
     public void getRoomSched(){
         assertThat(sc.getRoomSched("ROOM1", "12122019"), equalTo("10:30-13:30;14:30-15:30"));
     }
 
+    @Category({FastTest.class})
     @Test
     public void bookRoom(){//probs needs more test cases involving the db
         assertThat(sc.bookRoom("ROOM1", "12122019", "10:30-12:30"), equalTo(true));
     }
 
+    @Category(FastTest.class)
     @Test
-    public void a(){
+    public void getWorkStationTest(){
         assertThat(sc.getWorkStation(), equalTo("WORK1"));
     }
 }
