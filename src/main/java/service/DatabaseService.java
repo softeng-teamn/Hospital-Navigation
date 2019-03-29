a02a463ea73e3183cb03a74e63ceecacd7d8875a
Nick-Pingal <npingal@wpi.edu>
2019-03-28 23:13:20 -0400

    fixed static issues and other things

--- src/main/java/service/DatabaseService.java
+++ src/main/java/service/DatabaseService.java
@@ -9,38 +9,62 @@ import java.util.Collection;
 
 public class DatabaseService {
 
-    private static Connection connection;
+    private Connection connection;
 
-    public static void init() throws SQLException {
+    public DatabaseService(Connection connection) {
+        this.connection = connection;
+    }
+
+    public static DatabaseService init(String DBName) throws SQLException {
         DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
-        connection = DriverManager.getConnection("jdbc:derby:HospitalDB;create=true");
+        Connection connection = DriverManager.getConnection("jdbc:derby:"+DBName+";create=true");
+        DatabaseService myDB = new DatabaseService(connection);
+        myDB.createTables();
+        return myDB;
+    }
+
+    public static DatabaseService init() throws SQLException{
+        DatabaseService myDB = init("hospital-db");
+        return myDB;
     }
 
     // create tables in the database if they do not already exist.
-    public void createTables() throws SQLException{
-        DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
-        connection = DriverManager.getConnection("jdbc:derby:HospitalDB;create=true");
-        Statement statement = connection.createStatement();
-        // Check to see if the tables have already been created, if they have, do not create them a second time.
-        if(!tableExists("NODE")){
-            statement.execute("CREATE TABLE NODE (nodeID varchar(255) PRIMARY KEY, xcoord int, ycoord int, floor varchar(255), building varchar(255), nodeType varchar(255), longName varchar(255), shortName varchar(255))");
+    private void createTables(){
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            // Check to see if the tables have already been created, if they have, do not create them a second time.
+            if(!tableExists("NODE")){
+                statement.execute("CREATE TABLE NODE (nodeID varchar(255) PRIMARY KEY, xcoord int, ycoord int, floor varchar(255), building varchar(255), nodeType varchar(255), longName varchar(255), shortName varchar(255))");
+            }
+            if(!tableExists("EDGE")){
+                statement.execute("CREATE TABLE EDGE(edgeID varchar(21) PRIMARY KEY, node1 varchar(10), node2 varchar(10))");
+            }
+            if(!tableExists("EMPLOYEE")){
+                statement.execute("CREATE TABLE EMPLOYEE(employeeID int PRIMARY KEY, job varchar(25), isAdmin boolean)");
+            }
+            if(!tableExists("SERVICEREQUEST")){
+                statement.execute("CREATE TABLE SERVICEREQUEST(serviceID int PRIMARY KEY, serviceType varchar(4), locationNode varchar(10), description varchar(300), requestorID int, fulfillerID int)");
+            }
+            if(!tableExists("RESERVEDEVENT")){
+                statement.execute("CREATE TABLE RESERVEDEVENT(eventID int PRIMARY KEY, eventName varchar(50), locationID varchar(30), startTime int, endTime int, privacyLevel int, employeeID int)");
+            }
+            if(!tableExists("RESERVABLESPACE")){
+                statement.execute("CREATE TABLE RESERVABLESPACE(spaceID varchar(30) PRIMARY KEY , spaceName varchar(50), spaceType varchar(4), locationNode varchar(10), timeOpen timestamp, timeClosed timestamp)");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if(statement != null){
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        if(!tableExists("EDGE")){
-            statement.execute("CREATE TABLE EGDE(edgeID varchar(21) PRIMARY KEY, node1 varchar(10), node2 varchar(10))");
-        }
-        if(!tableExists("EMPLOYEE")){
-            statement.execute("CREATE TABLE EMPLOYEE(employeeID int PRIMARY KEY, job varchar(25), isAdmin boolean)");
-        }
-        if(!tableExists("SERVICEREQUEST")){
-            statement.execute("CREATE TABLE SERVICEREQUEST(serviceID int PRIMARY KEY, serviceType varchar(4), locationNode varchar(10), description varchar(300), requestorID int, fulfillerID int)");
-        }
-        if(!tableExists("RESERVEDEVENT")){
-            statement.execute("CREATE TABLE RESERVEDEVENT(eventID int PRIMARY KEY, eventName varchar(50), locationID varchar(30), startTime int, endTime int, privacyLevel int, employeeID int)");
-        }
-        if(!tableExists("RESERVABLESPACE")){
-            statement.execute("CREATE TABLE RESERVABLESPACE(spaceID varchar(30) PRIMARY KEY , spaceName varchar(50), spaceType varchar(4), locationNode varchar(10), timeOpen timestamp, timeClosed timestamp)");
-        }
-        statement.close();
+
+
 
 
 
@@ -69,7 +93,7 @@ public class DatabaseService {
     }
 
     // get edges from a specific floor
-    public static Collection<Edge> getEdges(int floor) {
+    public static Collection<Edge> getEdges(String floor) {
 
         ArrayList<Edge> edges = new ArrayList<>();
         Node a = new Node(0,0);
@@ -92,7 +116,7 @@ public class DatabaseService {
     }
 
     //oi sam how the f does this work
-    public boolean tableExists(String table){
+    boolean tableExists(String table){
         DatabaseMetaData dbm;
         try {
             dbm = connection.getMetaData();
@@ -101,6 +125,14 @@ public class DatabaseService {
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
+        }
+    }
+
+     void close(){
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
--- src/test/java/service/DatabaseServiceTest.java
+++ src/test/java/service/DatabaseServiceTest.java
@@ -1,34 +1,33 @@
 package service;
 
-import model.Node;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import testclassifications.FastTest;
 
+import javax.xml.crypto.Data;
 import java.sql.SQLException;
 
 import static org.junit.Assert.*;
 
 public class DatabaseServiceTest {
-/**
-    //@Before
+    DatabaseService myDB;
+
+    @Before
     public void setUp(){
 
         try {
-            DatabaseService.init();
+            myDB = DatabaseService.init();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-*/
-    //@After
-   // public void tearDown() throws Exception {
-   //     DatabaseService.getMyDBC().dropAll();
-   //     close();
-   // }
+
+    @After
+    public void tearDown() throws Exception {
+        myDB.close();
+    }
 
     @Test
     @Category(FastTest.class)
@@ -64,5 +63,20 @@ public class DatabaseServiceTest {
 
     @Test
     public void getEdges() {
+    }
+
+    // uh i legit don't know how to test this because everything relies on it and we can't delete
+    // the tables yet
+    @Test
+    public void createTables() {
+    }
+
+    @Test
+    @Category(FastTest.class)
+    public void tableExists() {
+        assertTrue(myDB.tableExists("NODE"));
+        assertFalse(myDB.tableExists("NOTPRESENT"));
+
+
     }
 }
