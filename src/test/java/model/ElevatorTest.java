6e6f0107785db3be1db5e1af59d0e8bfb23ea4cd
Lauren Hatfield <lhatfield15@gmail.com>
2019-04-02 12:51:15 -0400

    Elevator debugging

---
+++ src/test/java/model/ElevatorTest.java
@@ -0,0 +1,34 @@
+package model;
+
+import model.Elevator;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import testclassifications.FastTest;
+
+public class ElevatorTest {
+
+    Elevator e;
+
+    public ElevatorTest()  {
+    }
+
+    @Before
+    public void setup()throws Exception{
+        System.out.println("Searching for robots!");
+        e = Elevator.get("MyRobotName");
+    }
+    @After
+    public void shutdown(){
+        e.disconnect();
+    }
+    @Test
+    @Category(FastTest.class)
+    public void connectionTest() throws Exception {
+        double[] d = e.data;
+        System.out.println(d[0]);
+        Thread.sleep(100);
+        System.out.println(d[0]);
+    }
+}
