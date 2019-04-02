1bfe6e81adf4c855e4b8bb28f26d803e8e581e1c
Tyler Ferrara <tylerferrara123@gmail.com>
2019-04-02 00:08:21 -0400

    fixed home resource load

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(ResourceLoader.request);
+        Parent root = FXMLLoader.load(ResourceLoader.home);
         StageManager.changeWindow(primaryStage, root, "Home");
 
         CSVController.importIfNecessary();
