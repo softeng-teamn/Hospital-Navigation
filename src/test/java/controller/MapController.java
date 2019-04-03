c0daa13c1671d27eea4081864ab0bf2ae2c81aac
Nicole Cotto <nicolecotto316@gmail.com>
2019-04-02 21:55:40 -0400

    basic tests

---
+++ src/test/java/controller/MapController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import model.MapNode;
+import model.Node;
+import service.MapService;
+
+import java.util.ArrayList;
+
+public class MapController extends controller.Controller {
+
+    static MapService map;
+
+    // Creates static variables when compiled
+    static {
+        map = new MapService();
+    }
+
+    // Initializes the Map ???? if static method works, then this is not needed.
+    // Question for implementor: How will we know when the map needs to be built?
+    public void init() {
+
+    }
+
+
+    public static ArrayList<Node> getNodesConnectedTo(Node n) {
+        return controller.Controller.dbs.getNodesConnectedTo(n);
+    }
+
+
+}
