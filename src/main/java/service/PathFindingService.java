e0d8d18464b634f451ae553a3e847f64c779ae5f
Nathanalphaman <Github@magior.com>
2019-04-02 23:39:46 -0400

    request update

--- src/main/java/service/PathFindingService.java
+++ src/main/java/service/PathFindingService.java
@@ -22,8 +22,8 @@ public class PathFindingService {
         if (target != null) {
             ArrayList<Node> path = new ArrayList<Node>();
             while (target != null) { // INFINITE LOOP
-                System.out.println("im still in the loop");
-                System.out.println(target.getData().getNodeID());
+                //System.out.println("im still in the loop");
+                //System.out.println(target.getData().getNodeID());
                 // add every item to beginning of list
                 path.add(0, target.getData());
                 target = target.getParent();
@@ -39,51 +39,51 @@ public class PathFindingService {
     MapNode aStar(MapNode start, MapNode dest) {
         //1.  Initialize queue and set
         PriorityQueue<MapNode> open = new PriorityQueue<>();
-        System.out.println("Created open PriorityQueue");
+        //System.out.println("Created open PriorityQueue");
         Set<MapNode> explored = new HashSet<MapNode>();
         //2. Set up default values
         start.setG(0);
         open.add(start);
         while(!open.isEmpty()){
-            System.out.println(open.toString());
+            //System.out.println(open.toString());
             MapNode current = open.poll();
-            System.out.println("Current Node: " + current.getData().getNodeID());
+            //System.out.println("Current Node: " + current.getData().getNodeID());
             explored.add(current);
-            System.out.println("Added current node to explored Set");
+            //System.out.println("Added current node to explored Set");
             if(current.equals(dest)){
-                System.out.println("DESTINATION FOUND!!!!!");
+                //System.out.println("DESTINATION FOUND!!!!!");
                 return current;
             }
-            System.out.println("Iterating through children of current...");
+            //System.out.println("Iterating through children of current...");
             for (MapNode child : getChildren(current)){
-                System.out.println("child: " + child.getData().getNodeID());
+                //System.out.println("child: " + child.getData().getNodeID());
                 child.calculateG(current);
                 child.calculateHeuristic(dest);
                 double cost = current.getG() + child.getG() + child.getH();
 
                 if (child.equals(dest)) {
-                    System.out.println("This child is our destination node!");
+                    //System.out.println("This child is our destination node!");
                     child.setParent(current, current.getG() + child.getG());
                     return child;
                 }
 
                 if(open.contains(child) && cost>=child.getF()) {
-                    System.out.println("skipping this node because it was already seen");
+                    //System.out.println("skipping this node because it was already seen");
                     continue;
                 }
 
                 if(explored.contains(child) && cost>=child.getF()) {
-                    System.out.println("skipping this node because the cost is to big");
+                    //System.out.println("skipping this node because the cost is to big");
                     continue;
                 }
 
                 else if(!open.contains(child) || cost < child.getF()){
-                    System.out.println("setting child's parent to be current");
+                    //System.out.println("setting child's parent to be current");
                     child.setParent(current, current.getG() + child.getG());
                     if(open.contains(child)){
                         open.remove(child);
                     }
-                    System.out.println("adding child to open list");
+                    //System.out.println("adding child to open list");
                     open.add(child);
                 }
             }
@@ -101,6 +101,3 @@ public class PathFindingService {
         return nodeChildren;
     }
 }
-
-
-
