93ba286e80b347a657428110a264ed09e9469287
kimijiang <1017938873@qq.com>
2019-04-02 21:42:43 -0400

    update log in ui

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
