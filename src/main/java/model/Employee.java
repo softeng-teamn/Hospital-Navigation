c214ae73f0fed6ea0be9f1eeca5b521e0dca2243
JWTER7 <34319250+JWTER7@users.noreply.github.com>
2019-04-02 22:03:06 -0400

    ImportEmployees & ExportEmployees with Test added Employee class .ToString() add employees.csv added

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -61,4 +61,14 @@ public class Employee {
     public int hashCode() {
         return Objects.hash(ID, job, isAdmin);
     }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "ID=" + ID +
+                ", job='" + job + '\'' +
+                ", isAdmin=" + isAdmin +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
