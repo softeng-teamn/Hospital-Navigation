01e910ebe69f24929a89ace5c535a3fc9f3778c8
Tyler Ferrara <tylerferrara123@gmail.com>
2019-04-05 18:13:31 -0400

    deleting everything

---
+++ src/main/java/controller/MapView.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MapView {
+
+    void initialize() {
+
+    }
+
+    // ELEVATOR CALL BUTTONS
+    @FXML
+    void callElevatorAction(ActionEvent e) {
+
+    }
+}
