9000f0cb8b8e1f5dc5e3a9f2f1527574ca31a721
kimijiang <1017938873@qq.com>
2019-04-06 17:19:33 -0400

    link addnode, hide button, add wheal chair, change of log icon

--- src/main/java/controller/TopNav.java
+++ src/main/java/controller/TopNav.java
@@ -4,6 +4,9 @@ import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXToggleNode;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -22,9 +25,13 @@ public class TopNav {
     private EventBus eventBus = EventBusFactory.getEventBus();
 
     @FXML
-    private JFXButton navigate_btn, fulfillBtn, auth_btn, bookBtn, edit_btn;
+    private JFXButton navigate_btn, fulfillBtn, auth_btn, bookBtn, edit_btn, newNode_btn;
     @FXML
     private JFXTextField search_bar ;
+    @FXML
+    private JFXToggleNode accessibilityButton;
+    @FXML
+    private FontAwesomeIconView lock_icon;
 
     // events I send out/control
     @FXML
@@ -40,6 +47,13 @@ public class TopNav {
             Stage stage = (Stage) navigate_btn.getScene().getWindow();
             StageManager.changeExistingWindow(stage, root, "Admin Login");
         }
+    }
+
+    @FXML
+    void showNewNode(ActionEvent e) throws  Exception{
+        Parent root = FXMLLoader.load(ResourceLoader.createNode);
+        Stage stage = (Stage) newNode_btn.getScene().getWindow();
+        StageManager.changeExistingWindow(stage, root, "Add Node");
     }
 
 
@@ -76,14 +90,12 @@ public class TopNav {
 
         // SHOULD THIS GO HERE? (was in intialize of old map controller)
         navigate_btn.setVisible(false);
+        accessibilityButton.setVisible(false);
 
         resetBtn();
 
     }
 
-    void handleFindPath() {
-
-    }
 
 
     // events I care about: am "subscribed" to
@@ -96,7 +108,7 @@ public class TopNav {
                 // show navigation button
                 // navigate_btn.setVisible(true);
                 //showNavigationBtn(event);
-                showNavigationBtn(newEvent.getNodeSelected());        // will make nav btn visible, fill search with node
+                nodeSelectedHandler(newEvent.getNodeSelected());        // will make nav btn visible, fill search with node
                 break;
 
             case "login":     // receives from AdminLoginContoller?
@@ -110,9 +122,13 @@ public class TopNav {
         if(event.isAdmin()){
             fulfillBtn.setVisible(true);
             edit_btn.setVisible(true);
+            newNode_btn.setVisible(true);
+            lock_icon.setIcon(FontAwesomeIcon.SIGN_OUT);
         } else {
             fulfillBtn.setVisible(false);
             edit_btn.setVisible(false);
+            newNode_btn.setVisible(false);
+            lock_icon.setIcon(FontAwesomeIcon.SIGN_IN);
         }
     }
 
@@ -135,9 +151,11 @@ public class TopNav {
     //      show navigation button
     //      show node-selected in search
     @FXML
-    void showNavigationBtn(Node selected) {
+    void nodeSelectedHandler(Node selected) {
         // make change
         navigate_btn.setVisible(true);
+        accessibilityButton.setVisible(true);
+
 
         // show node-selected in search
         String fillNodeinSearch = selected.getLongName();
@@ -147,8 +165,9 @@ public class TopNav {
 
     public void startNavigation(ActionEvent actionEvent) {
         Event sendEvent = new Event();
+        Boolean accessibility = accessibilityButton.isSelected();
         sendEvent.setNodeSelected(event.getNodeSelected());
-        sendEvent.setAccessiblePath(false);
+        sendEvent.setAccessiblePath(accessibility);
         sendEvent.setEventName("navigation");
         eventBus.post(sendEvent);
     }
