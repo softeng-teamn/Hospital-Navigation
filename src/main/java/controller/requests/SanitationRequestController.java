ba1c92747d736af9bb7f2b2c29ba9d108e3d0f14
Masun77 <masun@wpi.edu>
2019-04-07 21:02:52 -0400

    complete - note error message and clearing input fields

--- src/main/java/controller/requests/SanitationRequestController.java
+++ src/main/java/controller/requests/SanitationRequestController.java
@@ -8,10 +8,12 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import model.request.MaintenanceRequest;
 import model.request.SanitationRequest;
 import service.DatabaseService;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,10 +27,14 @@ public class SanitationRequestController extends RequestController {
     @FXML
     private JFXButton submitBtn;
 
+    @FXML
+    private Label errorLbl;
+
     static DatabaseService myDBS = DatabaseService.getDatabaseService();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        errorLbl.setVisible(false);
         // Set urgency options
         ObservableList<String> options =
                 FXCollections.observableArrayList(
@@ -51,9 +57,18 @@ public class SanitationRequestController extends RequestController {
 
     @FXML
     void submitRequest(ActionEvent event) {
+        errorLbl.setVisible(false);
+        System.out.println("clicked");
         if(selectedNode != null) {
+            System.out.println("making request");
             SanitationRequest sanitationRequest = new SanitationRequest(-1, notes.getText(), selectedNode, false, urgencyBox.getValue(), materialBox.getValue());
             sanitationRequest.makeRequest();
+            notes.setText("");
+            urgencyBox.getSelectionModel().clearSelection();
+            materialBox.getSelectionModel().clearSelection();
+        }
+        else {
+            errorLbl.setVisible(true);
         }
     }
 
