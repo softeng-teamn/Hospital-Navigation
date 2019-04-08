2e7eb54a17e85ed4f5ddb5206f0443dd332d4403
samgoldman <sgoldman216@gmail.com>
2019-04-08 10:39:38 -0400

    Fix more capitalization

--- src/main/java/service/ResourceLoader.java
+++ src/main/java/service/ResourceLoader.java
@@ -15,7 +15,7 @@ public class ResourceLoader {
     public static final URL sanitationRequest = service.ResourceLoader.class.getResource("/requests/sanitationRequest.fxml");
     public static final URL ToyRequest = service.ResourceLoader.class.getResource("/requests/ToyRequest.fxml");
     public static final URL securityRequest = service.ResourceLoader.class.getResource("/requests/securityRequest.fxml");
-    public static final URL giftStoreRequest = service.ResourceLoader.class.getResource("/Requests/giftStoreRequest.fxml");
+    public static final URL giftStoreRequest = service.ResourceLoader.class.getResource("/requests/giftStoreRequest.fxml");
     public static final URL medicineRequest = service.ResourceLoader.class.getResource("/requests/medicine_request.fxml");
     public static final URL itRequest = service.ResourceLoader.class.getResource("/requests/it_request.fxml");
 
