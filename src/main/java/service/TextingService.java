a0e39aa0b5c4f902f391e28c4fb40e153601228f
mattheus98 <mcnascimento@wpi.edu>
2019-04-09 21:50:08 -0400

    Admin Login Update. Texting Service updated to pull from text file.

--- src/main/java/service/TextingService.java
+++ src/main/java/service/TextingService.java
@@ -4,26 +4,43 @@ import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class TextingService {
     // Find your Account Sid and Auth Token at twilio.com/console
-    public static final String ACCOUNT_SID =
-            "AC71f0446a3ec9e6790458fa5f4739730d";
-    public static final String AUTH_TOKEN =
-            "2bcd148080c10578255d1091762d0f07";
-    String phone;
-
-    //Add map when available
-    TextingService(String phone){
-        this.phone = phone;
+    private static String readLineByLineJava8(String filePath)
+    {
+        StringBuilder contentBuilder = new StringBuilder();
+        try (Stream<String> stream = Files.lines( Paths.get(filePath), StandardCharsets.UTF_8))
+        {
+            stream.forEach(s -> contentBuilder.append(s).append("\n"));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return contentBuilder.toString();
     }
+    String superSecret = readLineByLineJava8("/resources/SuperSecretPasswords.txt");
+    String[] secrets = superSecret.split("\n",2);
 
-    public void textMap(){
+    public final String ACCOUNT_SID =
+            secrets[0];
+    public final String AUTH_TOKEN =
+            secrets[1];
+
+    public void textMap(String phone, String mapUrl){
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
         Message message = Message
                 .creator(new com.twilio.type.PhoneNumber(phone), // to
                         new com.twilio.type.PhoneNumber("+19783961788"), // from
-                        "Greetings Father")
+                        mapUrl)
                 .create();
 
         System.out.println(message.getSid());
