b19f97e1712a1bfd8d16b684378824ab78f40854
Tyler Ferrara <tylerferrara123@gmail.com>
2019-04-14 01:13:18 -0400

    added gestureFX to createNode

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -43,7 +43,7 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(ResourceLoader.home);
+        Parent root = FXMLLoader.load(ResourceLoader.createNode);
         //
         // Closes all threads
         primaryStage.setOnCloseRequest((ae) -> {
