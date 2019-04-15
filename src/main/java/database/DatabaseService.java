fcf18e845e4b74916a6269fd191d44a81659626c
samgoldman <sgoldman216@gmail.com>
2019-04-15 13:56:32 -0400

    Added image support to employees

--- src/main/java/database/DatabaseService.java
+++ src/main/java/database/DatabaseService.java
@@ -49,7 +49,7 @@ public class DatabaseService {
     private ArrayList<Function<Void, Void>> edgeCallbacks;
     public boolean createFlag;
 
-    public Connection getConnection() {
+    Connection getConnection() {
         return connection;
     }
 
@@ -223,7 +223,7 @@ public class DatabaseService {
 
             statement.addBatch("CREATE TABLE EDGE(edgeID varchar(21) PRIMARY KEY, node1 varchar(255), node2 varchar(255))");
 
-            statement.addBatch("CREATE TABLE EMPLOYEE(employeeID int PRIMARY KEY, username varchar(255) UNIQUE, job varchar(25), isAdmin boolean, password varchar(50), phone varchar(255), email varchar(255), CONSTRAINT chk_job CHECK (job IN ('ADMINISTRATOR', 'DOCTOR', 'NURSE', 'JANITOR', 'SECURITY_PERSONNEL', 'MAINTENANCE_WORKER', 'IT', 'GUEST', 'RELIGIOUS_OFFICIAL', 'GIFT_SERVICES', 'MISCELLANEOUS', 'AV', 'INTERPRETER', 'TOY', 'PATIENT_INFO', 'FLORIST', 'INTERNAL_TRANSPORT', 'EXTERNAL_TRANSPORT')))");
+            statement.addBatch("CREATE TABLE EMPLOYEE(employeeID int PRIMARY KEY, username varchar(255) UNIQUE, job varchar(25), isAdmin boolean, password varchar(50), phone varchar(255), email varchar(255), image blob(600K), CONSTRAINT chk_job CHECK (job IN ('ADMINISTRATOR', 'DOCTOR', 'NURSE', 'JANITOR', 'SECURITY_PERSONNEL', 'MAINTENANCE_WORKER', 'IT', 'GUEST', 'RELIGIOUS_OFFICIAL', 'GIFT_SERVICES', 'MISCELLANEOUS', 'AV', 'INTERPRETER', 'TOY', 'PATIENT_INFO', 'FLORIST', 'INTERNAL_TRANSPORT', 'EXTERNAL_TRANSPORT')))");
 
             statement.addBatch("CREATE TABLE RESERVATION(eventID int PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 0, INCREMENT BY 1), eventName varchar(50), spaceID varchar(30), startTime timestamp, endTime timestamp, privacyLevel int, employeeID int)");
             statement.addBatch("CREATE TABLE RESERVABLESPACE(spaceID varchar(30) PRIMARY KEY, spaceName varchar(50), spaceType varchar(4), locationNode varchar(10), timeOpen timestamp, timeClosed timestamp)");
@@ -541,8 +541,7 @@ public class DatabaseService {
      * @return true if the insert succeeded or false if otherwise.
      */
     public boolean insertEmployee(Employee employee) {
-        String insertStatement = ("INSERT INTO EMPLOYEE VALUES(?, ?, ?, ?, ?, ?, ?)");
-        return executeInsert(insertStatement, employee.getID(), employee.getUsername(), employee.getJob().name(), employee.isAdmin(), employee.getPassword(), employee.getPhone(), employee.getEmail());
+        return employeeDatabase.insertEmployee(employee);
     }
 
     /**
--- src/main/java/database/EmployeeDatabase.java
+++ src/main/java/database/EmployeeDatabase.java
@@ -2,6 +2,10 @@ package database;
 
 import employee.model.Employee;
 
+import java.io.InputStream;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class EmployeeDatabase {
@@ -35,8 +39,8 @@ public class EmployeeDatabase {
      * @return true if the update succeeds and false if otherwise
      */
     boolean updateEmployee(Employee employee) {
-        String query = "UPDATE EMPLOYEE SET username=?, job=?, isAdmin=?, phone=?, email=? WHERE (employeeID = ?)";
-        return databaseService.executeUpdate(query, employee.getUsername(), employee.getJob().name(), employee.isAdmin(), employee.getPhone(), employee.getEmail(), employee.getID());
+        String query = "UPDATE EMPLOYEE SET username=?, job=?, isAdmin=?, password=?, phone=?, email=? WHERE (employeeID = ?)";
+        return databaseService.executeUpdate(query, employee.getUsername(), employee.getJob().name(), employee.isAdmin(), employee.getPassword(), employee.getPhone(), employee.getEmail(), employee.getID());
     }
 
     /**
@@ -57,5 +61,53 @@ public class EmployeeDatabase {
     Employee getEmployeeByUsername(String username) {
         String query = "SELECT * FROM EMPLOYEE WHERE (username = ?)";
         return (Employee) databaseService.executeGetById(query, Employee.class, username);
+    }
+
+    /**
+     * @param employee the employee to insert into the database
+     * @return true if the insert succeeded or false if otherwise.
+     */
+    boolean insertEmployee(Employee employee) {
+        String insertStatement = ("INSERT INTO EMPLOYEE(employeeID, username, job, isAdmin, password, phone, email) VALUES(?, ?, ?, ?, ?, ?, ?)");
+        return databaseService.executeInsert(insertStatement, employee.getID(), employee.getUsername(), employee.getJob().name(), employee.isAdmin(), employee.getPassword(), employee.getPhone(), employee.getEmail());
+    }
+
+    boolean updateEmployeeImage(int employeeId, InputStream stream) {
+        String query = "UPDATE EMPLOYEE SET image=? WHERE (employeeID = ?)";
+        return databaseService.executeUpdate(query, stream, employeeId);
+    }
+
+    InputStream getEmployeeImage(int employeeId) {
+        String query = "SELECT image FROM EMPLOYEE WHERE employeeID = ?";
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            stmt = databaseService.getConnection().prepareStatement(query);
+
+            databaseService.prepareStatement(stmt, employeeId);
+
+            // execute the query
+            rs = stmt.executeQuery();
+
+            boolean hasNext = rs.next();
+
+            // If there is no next node, return null
+            if (!hasNext) {
+                return null;
+            }
+
+            InputStream stream = rs.getBinaryStream("image");
+
+            stmt.close();
+            rs.close();
+
+            return stream;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            databaseService.closeAll(stmt, rs);
+        }
+        return null;
     }
 }
