136669d26ca807a7ee9cda0462a502f60071230a
cakocienda <cakocienda@wpi.edu>
2019-04-15 21:53:33 -0400

    Implemented login screen, fxml, and controller

---
+++ src/main/java/employee/controller/EnterApplicationController.java
@@ -0,0 +1,50 @@
+package employee.controller;
+
+import application_state.ApplicationState;
+import application_state.Event;
+import application_state.Observer;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import service.ResourceLoader;
+import service.StageManager;
+
+import java.io.IOException;
+
+import static javafx.fxml.FXMLLoader.load;
+
+public class EnterApplicationController{
+
+    //private Event event = ApplicationState.getApplicationState().getObservableBus().getEvent();
+
+    @FXML
+    StackPane sp = new StackPane();
+
+    @FXML
+    public void initialize() throws Exception{
+        sp.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event){
+                try {
+                    enter();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void enter() throws Exception {
+        Stage stage = (Stage) sp.getScene().getWindow();
+        Parent root = load(ResourceLoader.home);
+        StageManager.changeExistingWindow(stage, root, "Scheduler");
+    }
+
+
+}
