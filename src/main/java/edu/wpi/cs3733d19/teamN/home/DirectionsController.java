6308b1fa18a22641c8bf07d1e85de35a9e450eba
Nathanalphaman <Github@magior.com>
2019-04-24 01:48:10 -0400

    changed qr scaling

--- src/main/java/edu/wpi/cs3733d19/teamN/home/DirectionsController.java
+++ src/main/java/edu/wpi/cs3733d19/teamN/home/DirectionsController.java
@@ -932,8 +932,8 @@ public class DirectionsController implements Observer {
     @FXML
     public void showQRCode() {
         if (qrCodeVbox.getMaxHeight() == 0) {
-            qrCodeVbox.setPrefHeight(750);
-            qrCodeVbox.setMaxHeight(750);
+            qrCodeVbox.setPrefHeight(350);
+            qrCodeVbox.setMaxHeight(350);
             qrView.setVisible(true);
         }
         else {
